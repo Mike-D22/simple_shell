cf77543ae0b6e6d35d86665a5a57386a89f383e3
Mike-D22 <qamp26@gmail.com>
2023-07-23 13:15:35 -0700

    Environment

--- memory.c
+++ memory.c
@@ -1,30 +1,22 @@
 #include"shell.h"
 
-
 /**
  * bfree -it frees a pointer and NULLs address
-
  * @ptr: address of the pointer to free
  *
-
  * Return: 1 if freed, otherwise 0.
  */
 
 int bfree(void **ptr)
 {
+	if (ptr && *ptr)
 
-        if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
 
-        {
+		return (1);
 
-                free(*ptr);
-
-                *ptr = NULL;
-
-                return (1);
-
-        }
-
-        return (0);
-
+	}
+	return (0);
 }
